5e05722c9121b74a2cbd305f6efb9399a7afe43f
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-03-16 16:07:47 +0530

    added all interfaces assignment

--- interfaces.ts
+++ interfaces.ts
@@ -1,18 +1,36 @@
-interface User{
-    email : string,
-    userId : number,
-    googleId? : string
-    startTrail : () => string,
-    getCoupon(couponname: number): number
+// Assignment #1 - Create a function isLegal that returns true or false if a user is above 18. It takes a user as an input.
+interface User {
+    firstName: string,
+    lastName: string,
+    email: string,
+    age: number
+}
+function isLegal(user: User){
+    if(user.age >= 18){
+        return true;
+    } else{
+        return false;
+    }
 }
 
-const Aditya : User = {
-    email : "fiwio",
-    userId : 22114,
-    startTrail: () => {
-        return "trail started"
-    },
-    getCoupon : (couponname : 20) => {
-        return 10 + couponname;
-    }
+// Assignment #2 - Create a React component that takes todos as an input and renders them
+
+interface TodoType {
+    title: string,
+    description: string,
+    done: boolean
+}
+
+interface TodoInput {
+    todo: TodoType
+}
+
+function renderTodo({ todo }: TodoInput){
+    return (
+        <div>
+            <h1>{todo.title}</h1>
+            <h2>{todo.description}</h2>
+        </div>
+    )
+
 }
